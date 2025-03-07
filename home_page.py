5b36bfc54bd3b5fe12c64badbdf67933fe01b9b9
augustecrabeil <auguste.crabeil2001@gmail.com>
2025-03-07 17:57:27 +0100

    un délire de canard

--- home_page.py
+++ home_page.py
@@ -2,6 +2,7 @@ from utils import Button, Font, load_images
 from config import SCREEN_WIDTH, SCREEN_HEIGHT
 
 import pygame
+import random
 
 DUCK_IMAGES = load_images("background", ["duck_1", "duck_2", "duck_3"])
 
@@ -12,8 +13,21 @@ class HomePage:
         self.background = pygame.image.load("images/home_page.jpg")
         self.background = pygame.transform.scale(self.background, (SCREEN_WIDTH, SCREEN_HEIGHT))
         self.time = 0
-        self.current_image = 1
+
+        self.time = 0
         self.duration_image = 6
+        self.num_ducks = 6
+        self.ducks = []
+
+        for _ in range(self.num_ducks):
+            duck = {
+                "current_image": random.randint(1, 3), 
+                "duck_x": SCREEN_WIDTH + random.randint(50, 2000), 
+                "duck_y": random.randint(50, 200), 
+                "duck_speed": random.randint(3, 5),
+                "time": random.randint(0, 10),
+            }
+            self.ducks.append(duck)
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -21,16 +35,24 @@ class HomePage:
                 return self.button.callback()
         return False
     
-    def update_image(self):
-        self.time += 1
-        self.current_image = (self.time//self.duration_image) % 3 + 1
+    def update_ducks(self):
+        for duck in self.ducks:
+            duck["time"] += 1
+            duck["current_image"] = (duck["time"] // self.duration_image) % 3 + 1
+            duck["duck_x"] -= duck["duck_speed"]
+            
+            if duck["duck_x"] < -100:
+                duck["duck_x"] = SCREEN_WIDTH + random.randint(50, 200)
+                duck["duck_y"] = random.randint(50, 200)
+                duck["time"] = 0
     
     def display(self):
         self.screen.fill((255,255,255))
         self.screen.blit(self.background, (0, 0))
 
-        self.screen.blit(DUCK_IMAGES[f"duck_{self.current_image}"], (SCREEN_WIDTH - 100, 100))
-        self.update_image()
+        self.update_ducks()
+        for duck in self.ducks:
+            self.screen.blit(DUCK_IMAGES[f"duck_{duck['current_image']}"], (duck["duck_x"], duck["duck_y"]))
 
         self.screen.blit(Font.render("Welcome to super game", "large"), (SCREEN_WIDTH//2 - 140, 100))
         self.button.draw(self.screen, "medium")
